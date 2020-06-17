4b13660186f522b4b16525340db6ee3a7634d3fd
naohito muraoka <muraokan@oregonstate.edu>
2020-06-17 10:28:52 +0900

    追加: 複数Repository作成時のStruct、Controllerを追加

--- src/api/domain/repositories/create_repository.go
+++ src/api/domain/repositories/create_repository.go
@@ -1,9 +1,24 @@
 package repositories
 
+import (
+	"strings"
+
+	"../../utils/errors"
+)
+
 // CreateRepositoryRequest -
 type CreateRepositoryRequest struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
+}
+
+// Validate -
+func (r *CreateRepositoryRequest) Validate()errors.APIError {
+	r.Name = strings.TrimSpace(r.Name)
+	if r.Name == "" {
+		return errors.NewBadRequestError("invalid repository name")
+	}
+	return nil
 }
 
 // CreateRepositoryResponse -
@@ -11,4 +26,16 @@ type CreateRepositoryResponse struct {
 	ID    int64  `json:"id"`
 	Owner string `json:"owner"`
 	Name  string `json:"name"`
+}
+
+// CreateRepositoriesResponse -
+type CreateRepositoriesResponse struct {
+	StatusCode int                        `json:"status"`
+	Results    []CreateRepositoriesResult `json:"results"`
+}
+
+// CreateRepositoriesResult -
+type CreateRepositoriesResult struct {
+	Response CreateRepositoryResponse `json:"repo"`
+	Error    errors.APIError          `json:"error"`
 }
